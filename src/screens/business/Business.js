e8b878b1cd0866cb899e66784198cfd6f21d59ef
LongNguyen <ndlong@sdc.udn.vn>
2022-11-10 20:56:39 +0700

    add id router

--- src/screens/business/Business.js
+++ src/screens/business/Business.js
@@ -1,8 +1,30 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 export const Business = () =>{
+    const listData =[
+        {
+            id: 496,
+            name: "Doanh nghiep 1"
+        },
+        {
+            id: 497,
+            name: "Doanh nghiep 2"
+        },
+        {
+            id: 498,
+            name: "Doanh nghiep 3"
+        },
+    ]
     return (
         <>
-       List Business, abcd
+            {
+                listData.map((item, index)=>(
+                    <p>
+                        {item.name} 
+                        <Link to={"/business-detail/"+item.id}> Xem chi tiet</Link>
+                    </p>
+                ))
+            }
         </>
     )
 }
